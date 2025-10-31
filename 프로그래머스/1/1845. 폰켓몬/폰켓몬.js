0f91f6797a1cc689b7bc82cc76eeeba4f561fad7
kyuri-Kwon <68770864+rnjs010@users.noreply.github.com>
2025-10-31 21:16:10 +0900

    [level 1] Title: 폰켓몬, Time: 0.64 ms, Memory: 34.2 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.js
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.js
@@ -1,16 +1,8 @@
 function solution(nums) {
     var answer = nums.length / 2;
     
-    let map = new Map();
-    for (const i of nums) {
-        if (map.has(i)) {
-            map.set(i, map.get(i) + 1);
-        } else {
-            map.set(i, 1);
-        }
-    }
-    
-    answer = Math.min(answer, map.size);
+    const unique = new Set(nums);
+    answer = Math.min(answer, unique.size);
     
     return answer;
 }
