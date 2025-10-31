938763298822099a059cef9a2cda42049c790387
kyuri-Kwon <68770864+rnjs010@users.noreply.github.com>
2025-10-31 22:16:37 +0900

    [level 2] Title: 의상, Time: 0.23 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.js
+++ 프로그래머스/2/42578. 의상/의상.js
@@ -1,4 +1,15 @@
 function solution(clothes) {
-    var answer = 0;
-    return answer;
+    var answer = 1;
+    
+    const map = new Map();
+    for (let i = 0; i < clothes.length; i++) {
+        const type = clothes[i][1];
+        map.set(type, (map.get(type) || 0) + 1);
+    }
+    
+    for (const cnt of map.values()) {
+        answer *= (cnt + 1);
+    }
+    
+    return answer - 1;
 }
